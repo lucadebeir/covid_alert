f8d968a920682ca054defb618c079631a14b0d47
clement <clementemil@aol.fr>
2020-10-28 14:11:43 +0100

    First tests

--- src/main/java/com/polytechmtp/covidalert/repositories/UserRepository.java
+++ src/main/java/com/polytechmtp/covidalert/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.polytechmtp.covidalert.repositories;
 import com.polytechmtp.covidalert.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
 }
