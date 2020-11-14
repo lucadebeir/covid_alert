314dfca7f5712c82b71e3c60ed946405dd07af61
Matthew <haas.matthew@free.fr>
2020-11-14 13:00:19 +0100

    Kafka/Zookeeper added to docker-compose and linked to the spring boot application

---
+++ src/main/java/com/polytechmtp/covidalert/KafkaController.java
@@ -0,0 +1,22 @@
+package com.polytechmtp.covidalert;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    private final com.polytechmtp.covidalert.KafkaProducer producer;
+
+    public KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping("/publish")
+    public void writeMessageToTopic(@RequestParam("message") String message){
+        this.producer.writeMessage(message);
+
+    }
+
+}
